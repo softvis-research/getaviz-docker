e41d00f15059326a80fbba1a94e81a5769f43858
David Baum <david.baum@uni-leipzig.de>
2019-07-05 16:26:41 +0200

    set correct permissions for data-gen in start script

--- generator2/start.sh
+++ generator2/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-chown -R jetty:jetty /var/lib/jetty/logs/ /var/lib/jetty/output/
+chown -R jetty:jetty /var/lib/jetty/logs/ /var/lib/jetty/output/ /var/lib/jetty/data-gen/
 su jetty -c /docker-entrypoint.sh
